6d99ce2706f664eb430e10e2869dc721ba4ba13a
David Nicholson <nickledave@users.noreply.github.com>
2022-01-21 10:12:21 -0500

    CLN: add changes to `src/vocles/` made by pre-commit

--- src/vocles/dataset.py
+++ src/vocles/dataset.py
@@ -1,25 +1,23 @@
+import pathlib
 from collections import defaultdict
 from collections.abc import Sequence
-import pathlib
 
-from attrs import define, field
 import pandas as pd
+from attrs import define, field
 
 from .vocalization import Vocalization
 
 
 @define
 class Dataset:
-    COLUMNS = ('audio_path', 'spect_path', 'annot_path', 'annot_format')
+    COLUMNS = ("audio_path", "spect_path", "annot_path", "annot_format")
 
     vocalizations: Sequence[Vocalization] = field()
 
     @vocalizations.validator
     def are_all_vocalizations(self, attribute, value):
         if not all([isinstance(item, Vocalization) for item in value]):
-            raise ValueError(
-                'all items passed in must be a Vocalization'
-            )
+            raise ValueError("all items passed in must be a Vocalization")
 
     def to_df(self) -> pd.DataFrame:
         records = defaultdict(list)
--- src/vocles/vocalization.py
+++ src/vocles/vocalization.py
@@ -6,13 +6,21 @@ from attrs import converters, define, field
 
 @define
 class Vocalization:
-    audio_path: Optional[pathlib.Path] = field(converter=converters.optional(pathlib.Path), default=None)
-    spect_path: Optional[pathlib.Path] = field(converter=converters.optional(pathlib.Path), default=None)
-    annot_path: Optional[pathlib.Path] = field(converter=converters.optional(pathlib.Path), default=None)
-    annot_format: Optional[str] = field(converter=converters.optional(str), default=None)
+    audio_path: Optional[pathlib.Path] = field(
+        converter=converters.optional(pathlib.Path), default=None
+    )
+    spect_path: Optional[pathlib.Path] = field(
+        converter=converters.optional(pathlib.Path), default=None
+    )
+    annot_path: Optional[pathlib.Path] = field(
+        converter=converters.optional(pathlib.Path), default=None
+    )
+    annot_format: Optional[str] = field(
+        converter=converters.optional(str), default=None
+    )
 
     def __attrs_post_init__(self):
         if self.audio_path is None and self.spect_path is None:
             raise ValueError(
-                'a Vocalization must have either an audio_path or a spect_path, but both were None'
+                "a Vocalization must have either an audio_path or a spect_path, but both were None"
             )
